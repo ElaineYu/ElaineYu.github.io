9665406a42482c091d7f698b048058294864872c
Elaine Yu <elaineicecream@Elaines-MacBook-Pro.local>
2013-12-31 15:56:54 -0800

    added javascript file to show and hide about informational boxes

---
+++ javascripts/about_box.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#show").click(function(){
+    $("#about_project_box").show();
+  });
+  $("#hide").click(function(){
+    $("#about_project_box").hide();
+  });
+ 
+});
