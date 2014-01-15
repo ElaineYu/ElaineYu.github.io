a2482cd6b38b02c1f74a5bb0e46911cc71fceb41
Elaine Yu <elaineicecream@Elaines-MacBook-Pro.local>
2014-01-15 15:54:18 -0800

    adding gitignore and _site files

---
+++ _site/javascripts/about_box.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("#show").click(function(){
+    $("#about_braintrade_project_box").show();
+  });
+  $("#hide").click(function(){
+    $("#about_braintrade_project_box").hide();
+  });
+ 
+  $("#show1").click(function(){
+    $("#about_bootietracker_project_box").show();
+  });
+  $("#hide1").click(function(){
+    $("#about_bootietracker_project_box").hide();
+  });
+});
