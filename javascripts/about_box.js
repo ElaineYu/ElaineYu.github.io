b223cbe7ac361a994576d9a03b95ec03f03ac3e4
Elaine Yu <elaineicecream@Elaines-MacBook-Pro.local>
2014-01-15 15:46:46 -0800

    added hide and show functions for javascript

--- javascripts/about_box.js
+++ javascripts/about_box.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
   $("#show").click(function(){
-    $("#about_project_box").show();
+    $("#about_braintrade_project_box").show();
   });
   $("#hide").click(function(){
-    $("#about_project_box").hide();
+    $("#about_braintrade_project_box").hide();
   });
  
+  $("#show1").click(function(){
+    $("#about_bootietracker_project_box").show();
+  });
+  $("#hide1").click(function(){
+    $("#about_bootietracker_project_box").hide();
+  });
 });
