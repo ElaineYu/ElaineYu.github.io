b8a9595b3b5795339403a7a0ff979996033299fa
Elaine Yu <elaineicecream@Elaines-MacBook-Pro.local>
2014-01-15 20:07:26 -0800

    small changed to js

--- _site/javascripts/about_box.js
+++ _site/javascripts/about_box.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
         $("#about_braintrade_project_box").toggle();
 
       });
+    
     $('#show1').click(function(event){
        event.preventDefault();
        console.log("click!");
         $("#about_bootietracker_project_box").toggle();
 
       });
-
 });
