77c86a48642ccd370efba7047507b160877939f6
Elaine Yu <elaineicecream@Elaines-MacBook-Pro.local>
2014-01-20 16:46:42 -0800

    resume button, jQuery toggle

--- _site/javascripts/about_box.js
+++ _site/javascripts/about_box.js
@@ -2,15 +2,18 @@ $(document).ready(function(){
 
     $('#show').click(function(event){
        event.preventDefault();
-       console.log("click!");
         $("#about_braintrade_project_box").toggle();
 
       });
     $('#show1').click(function(event){
        event.preventDefault();
-       console.log("click!");
         $("#about_bootietracker_project_box").toggle();
 
       });
+
+    $('#show2').click(function(event){
+      event.preventDefault();
+        $('.resume').toggle();
+    });
 
 });
